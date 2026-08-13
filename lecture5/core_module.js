ebc0b475077f4b26c285724fd4dadfc7467d4af8
Hitarth Kumar Srivastava <hitarthsri@gmail.com>
2026-08-13 11:52:20 +0530

    ooh yeah

--- lecture5/core_module.js
+++ lecture5/core_module.js
@@ -1,9 +1,29 @@
-const os=require('os');
+// const os=require('os');
 // console.log(os.platform());
 // console.log(os.arch());
 // console.log(os.cpus().length);
 // console.log(os.homedir());
 
-console.log(os.totalmem()/(1024*1024*1024)
-+" GB"); // Convert to GB
-console.log(os.freemem()/(1024*1024*1024)+" GB"); // Convert to GB
+// console.log(os.totalmem()/(1024*1024*1024)
+// +" GB"); // Convert to GB
+// console.log(os.freemem()/(1024*1024*1024)+" GB"); // Convert to GB.
+
+const path=require('path');
+const fs=require('fs');
+
+const file="lecture5\\core_module.js";
+
+console.log(path.dirname(file)); // file Name
+console.log(path.basename(file));// core_module.js
+console.log(path.extname(file));// .js
+
+const data =fs.readFileSync("sample.txt","utf-8");
+console.log(data);
+
+fs.readFile("sample.txt","utf-8",(err,data)=>{
+    if(err){
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
